46d210582fc35d5bd30fd8ae8c707a7e1779b9f9
Rabindranath Ferreira <ferreiralox@gmail.com>
2024-03-19 18:30:30 +0100

    feat: :sparkles: create monster CRUD endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MonsterModule } from './monster/monster.module';
 
 @Module({
-  imports: [],
+  imports: [MonsterModule],
   controllers: [AppController],
   providers: [AppService],
 })

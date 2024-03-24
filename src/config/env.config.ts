666faad26b6415dfcbf43ae9be7987ba097c2155
Rabindranath Ferreira <ferreiralox@gmail.com>
2024-03-24 18:03:49 +0100

    build: :green_heart: update mongo url for production

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,6 @@
 export const loadConfig = () => ({
   NODE_ENV: process.env.NODE_ENV || 'development',
-  MONGO_URL:
-    process.env.NODE_ENV === 'production'
-      ? process.env.MONGO_URL
-      : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  MONGO_URL: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   PORT: +process.env.PORT || 3000,
   CACHE: {
     ttl: +process.env.TTL_CACHE,

ca4c087c6704c32397fb3415d2e895905af6326d
Rabindranath Ferreira <ferreiralox@gmail.com>
2024-03-25 09:11:39 +0100

    ci: :green_heart: finish dockerized process for dev and for production via docker compose

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configServ: ConfigService) {}
   getAlive(): any {
-    return { message: `API UP AND RUNNING` };
+    return {
+      message: `API UP AND RUNNING, in ENV: ${this.configServ.get('NODE_ENV')} and PORT: ${this.configServ.get('PORT')}`,
+    };
   }
 }
